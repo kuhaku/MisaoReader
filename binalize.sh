cf8177e44c926cf77efc2678dcdaee7cfdb0909d
Kuhaku <kuhaku.mami@gmail.com>
2017-07-15 20:13:14 +0900

    zip binaries

--- binalize.sh
+++ binalize.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -eu
 electron-packager . MisaoViewer --platform=darwin --arch=x64 --overwrite --electron-version=1.6.11
 electron-packager . MisaoViewer --platform=win32 --arch=x64 --overwrite --electron-version=1.6.11
+zip -r MisaoViewer-darwin-x64.zip MisaoViewer-darwin-x64
+zip -r MisaoViewer-win32-x64.zip MisaoViewer-win32-x64
