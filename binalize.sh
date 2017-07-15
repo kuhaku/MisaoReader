aa827ae7039ab9daa5e3b50bf4ef02fa116e5e9e
Kuhaku <kuhaku.mami@gmail.com>
2017-07-16 01:08:56 +0900

    Rename MisaoViewer -> MisaoReader

--- binalize.sh
+++ binalize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -eu
 electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=1.6.11
 electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=1.6.11
-zip -r MisaoReader-darwin-x64.zip MisaoViewer-darwin-x64
-zip -r MisaoReader-win32-x64.zip MisaoViewer-win32-x64
+zip -r MisaoReader-darwin-x64.zip MisaoReader-darwin-x64
+zip -r MisaoReader-win32-x64.zip MisaoReader-win32-x64
