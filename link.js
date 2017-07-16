45479b051619883598acf761f7285353cea1c71e
Kuhaku <kuhaku.mami@gmail.com>
2017-07-16 22:05:01 +0900

    Open link via external browser

---
+++ link.js
@@ -0,0 +1,7 @@
+var shell = require('electron').shell;
+
+//open links externally by default
+$(document).on('click', 'a[href^="http"]', function(event) {
+  event.preventDefault();
+  shell.openExternal(this.href);
+});
