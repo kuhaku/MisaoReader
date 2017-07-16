97d101fdbb8ca583cfd0a1a9faf49eb89508d11d
Kuhaku <kuhaku.mami@gmail.com>
2017-07-16 22:28:30 +0900

    remove binary files before generating

--- binalize.sh
+++ binalize.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -eu
 
+rm -rf MisaoReader-*
+
 electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=1.6.11 --icon icons/misao.icns
 electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=1.6.11  --icon icons/misao.ico
 
