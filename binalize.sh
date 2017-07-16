83534bb98a383c4a80a156797f3916972185c0cd
Kuhaku <kuhaku.mami@gmail.com>
2017-07-16 20:05:06 +0900

    Add application icon

--- binalize.sh
+++ binalize.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -eu
-electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=1.6.11
-electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=1.6.11
+
+electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=1.6.11 --icon icons/misao.icns
+electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=1.6.11  --icon icons/misao.ico
+
 zip -r MisaoReader-darwin-x64.zip MisaoReader-darwin-x64
 zip -r MisaoReader-win32-x64.zip MisaoReader-win32-x64
