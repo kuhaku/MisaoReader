095ec7dec0bce3cf074f886b618789b419eaa0c9
Kuhaku <kuhaku.mami@gmail.com>
2017-07-24 00:26:18 +0900

    eslint

--- src/fetch.js
+++ src/fetch.js
@@ -1,132 +1,132 @@
 const http = require('http');
 const querystring = require('querystring');
 const iconv = require('iconv-lite');
-var $ = jQuery = require("../node_modules/jquery/dist/jquery.slim.min.js");
-var pc = '0'
-var post_id = '0'
+var $ = jQuery = require('../node_modules/jquery/dist/jquery.slim.min.js');
+var pc = '0';
+var post_id = '0';
 
 function initialFetchBBS () {
-  const URL = 'http://misao.on.arena.ne.jp/cgi-bin/bbs.cgi';
-  http.get(URL, (res) => {
-    var chunks = [];
-    res.on('data', (chunk) => {
-      chunks.push(chunk);
-    });
+    const URL = 'http://misao.on.arena.ne.jp/cgi-bin/bbs.cgi';
+    http.get(URL, (res) => {
+        var chunks = [];
+        res.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
 
-    res.on('end', (res) => {
-      var body = iconv.decode(Buffer.concat(chunks), 'cp932');
-      body = body.replace(/<!--[\s\S]*?-->/g, '').replace(/\r\n/g, '\r').replace(/\n/g, '\r');
-      body = body.replace(/<A /g, '<A target="_blank" ');
-      pc = body.match(/name="pc" value="(\d+)"/)[1];
-      post_id = body.match(/name="p" value="(\d+)"/)[1];
-      for(var pre of body.split('<HR>').slice(4,-2)){
-        pre = findYouTube(pre);
-        pre = findImage(pre);
-        pre = replace(pre);
-        $('#posts').append('<HR>' + pre);
-      }
+        res.on('end', (res) => {
+            var body = iconv.decode(Buffer.concat(chunks), 'cp932');
+            body = body.replace(/<!--[\s\S]*?-->/g, '').replace(/\r\n/g, '\r').replace(/\n/g, '\r');
+            body = body.replace(/<A /g, '<A target="_blank" ');
+            pc = body.match(/name="pc" value="(\d+)"/)[1];
+            post_id = body.match(/name="p" value="(\d+)"/)[1];
+            for(var pre of body.split('<HR>').slice(4,-2)){
+                pre = findYouTube(pre);
+                pre = findImage(pre);
+                pre = replace(pre);
+                $('#posts').append('<HR>' + pre);
+            }
+        });
     });
-  });
 }
 
 function fetchBBS () {
-  const postItem = querystring.stringify({
-    pc: pc,
-    p: post_id,
-    m: 'p',
-    k: iconv.encode('あ', 'cp932'),
-    c: 100,
-    d: 0,
-    a: 'checked',
-    g: 'checked',
-    midokureload: iconv.encode('投稿／リロード', 'cp932')
-  });
-  const urlOpts = {
-    host: 'misao.on.arena.ne.jp',
-    path: '/cgi-bin/bbs.cgi',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': postItem.length
-    }
-  };
-
-  const req = http.request(urlOpts, function (res) {
-    var chunks = [];
-    res.on('data', function (chunk) {
-      chunks.push(chunk);
-    }).on('error', function (e) {
-      console.log('ERROR:' + e.stack);
+    const postItem = querystring.stringify({
+        pc: pc,
+        p: post_id,
+        m: 'p',
+        k: iconv.encode('あ', 'cp932'),
+        c: 100,
+        d: 0,
+        a: 'checked',
+        g: 'checked',
+        midokureload: iconv.encode('投稿／リロード', 'cp932')
     });
+    const urlOpts = {
+        host: 'misao.on.arena.ne.jp',
+        path: '/cgi-bin/bbs.cgi',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': postItem.length
+        }
+    };
 
-    res.on('end', (res) => {
-      var body = iconv.decode(Buffer.concat(chunks), 'cp932');
-      body = body.replace(/<!--[\s\S]*?-->/g, '').replace(/\r\n/g, '\r').replace(/\n/g, '\r');
-      body = body.replace(/<A /, '<A target="_blank" ')
-      pc = body.match(/name="pc" value="(\d+)"/)[1];
-      post_id = body.match(/name="p" value="(\d+)"/)[1];
-      for(var pre of body.split('<HR>').slice(4,-2).reverse()){
-        pre = replace(pre);
-        pre = findYouTube(pre);
-        pre = findImage(pre);
-        $('#posts').prepend('<HR>' + pre);
-      }
+    const req = http.request(urlOpts, function (res) {
+        var chunks = [];
+        res.on('data', function (chunk) {
+            chunks.push(chunk);
+        }).on('error', function (e) {
+            console.log('ERROR:' + e.stack);
+        });
+
+        res.on('end', (res) => {
+            var body = iconv.decode(Buffer.concat(chunks), 'cp932');
+            body = body.replace(/<!--[\s\S]*?-->/g, '').replace(/\r\n/g, '\r').replace(/\n/g, '\r');
+            body = body.replace(/<A /, '<A target="_blank" ');
+            pc = body.match(/name="pc" value="(\d+)"/)[1];
+            post_id = body.match(/name="p" value="(\d+)"/)[1];
+            for(var pre of body.split('<HR>').slice(4,-2).reverse()){
+                pre = replace(pre);
+                pre = findYouTube(pre);
+                pre = findImage(pre);
+                $('#posts').prepend('<HR>' + pre);
+            }
+        });
     });
-  });
-  req.write(postItem);
-  req.end();
-};
+    req.write(postItem);
+    req.end();
+}
 
 function findYouTube(pre) {
-  var youtube_id = pre.match(/.youtube.com\/watch\?v=([0-9a-zA-Z_\-]+)/);
-  if (youtube_id) {
-    pre += '<iframe width="360" height="215" src="https://www.youtube.com/embed/' + youtube_id[1] + '" frameborder="0" allowfullscreen></iframe>';
-  } else if (pre.match(/youtu.be\/([0-9a-zA-Z_]+)/)){
-    youtube_id = pre.match(/youtu.be\/([0-9a-zA-Z_\-]+)/)
-    if (youtube_id){
-      pre += '<iframe width="360" height="215" src="https://www.youtube.com/embed/' + youtube_id[1] + '" frameborder="0" allowfullscreen></iframe>';
+    var youtube_id = pre.match(/.youtube.com\/watch\?v=([0-9a-zA-Z_\-]+)/);
+    if (youtube_id) {
+        pre += '<iframe width="360" height="215" src="https://www.youtube.com/embed/' + youtube_id[1] + '" frameborder="0" allowfullscreen></iframe>';
+    } else if (pre.match(/youtu.be\/([0-9a-zA-Z_]+)/)){
+        youtube_id = pre.match(/youtu.be\/([0-9a-zA-Z_\-]+)/);
+        if (youtube_id){
+            pre += '<iframe width="360" height="215" src="https://www.youtube.com/embed/' + youtube_id[1] + '" frameborder="0" allowfullscreen></iframe>';
+        }
     }
-  }
-  return pre
+    return pre;
 }
 
 function findImage(pre) {
-  let bufArray = [];
-  let lines = pre.split('\r')
-  for (let i = 0; i < lines.length; i++ ) {
-    let misao_id = lines[i].match(/misao.on.arena.ne.jp\/c\/up\/misao([\d]+)\.jpg/);
-    if (misao_id) {
-      lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="http://misao.on.arena.ne.jp/c/up/pixy_misao' + misao_id[1] + '.jpg">' + lines[i];
-    } else if (lines[i].match(/pbs.twimg.com\/media\/([0-9a-zA-Z_]+)\.jpg/)) {
-      let twimg_id = lines[i].match(/pbs.twimg.com\/media\/([0-9a-zA-Z_]+)\.jpg/);
-      if (twimg_id) {
-        lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="https://pbs.twimg.com/media/' + twimg_id[1] + '.jpg:thumb">' + lines[i];
-      }
-    } else if (lines[i].match(/twitpic.com\/[0-9a-zA-Z_]+/)) {
-      let twitpic = lines[i].match(/twitpic.com\/[0-9a-zA-Z_]+/);
-      if (twitpic) {
-        lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="http://' + twitpic + '">' + lines[i];
-      }
-    } else if (lines[i].match(/(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+(jpg|gif|png))/)) {
-      let image = lines[i].match(/(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+(jpg|gif|png))/);
-      if (image) {
-        lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="' + image[1] + '">' + lines[i];
-      }
+    let bufArray = [];
+    let lines = pre.split('\r');
+    for (let i = 0; i < lines.length; i++ ) {
+        let misao_id = lines[i].match(/misao.on.arena.ne.jp\/c\/up\/misao([\d]+)\.jpg/);
+        if (misao_id) {
+            lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="http://misao.on.arena.ne.jp/c/up/pixy_misao' + misao_id[1] + '.jpg">' + lines[i];
+        } else if (lines[i].match(/pbs.twimg.com\/media\/([0-9a-zA-Z_]+)\.jpg/)) {
+            let twimg_id = lines[i].match(/pbs.twimg.com\/media\/([0-9a-zA-Z_]+)\.jpg/);
+            if (twimg_id) {
+                lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="https://pbs.twimg.com/media/' + twimg_id[1] + '.jpg:thumb">' + lines[i];
+            }
+        } else if (lines[i].match(/twitpic.com\/[0-9a-zA-Z_]+/)) {
+            let twitpic = lines[i].match(/twitpic.com\/[0-9a-zA-Z_]+/);
+            if (twitpic) {
+                lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="http://' + twitpic + '">' + lines[i];
+            }
+        } else if (lines[i].match(/(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+(jpg|gif|png))/)) {
+            let image = lines[i].match(/(https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+(jpg|gif|png))/);
+            if (image) {
+                lines[i] = '<img referrerpolicy="no-referrer" class="thumbnail" src="' + image[1] + '">' + lines[i];
+            }
+        }
+        bufArray.push(lines[i]);
     }
-    bufArray.push(lines[i]);
-  }
-  return bufArray.join('\r')
+    return bufArray.join('\r');
 }
 
 function replace (pre) {
-  return pre.replace(/&amp;#(\d+|x[0-9a-fA-F]+);/g, replaceCharacterEntity);
+    return pre.replace(/&amp;#(\d+|x[0-9a-fA-F]+);/g, replaceCharacterEntity);
 }
 
 function replaceCharacterEntity (str, p1) {
-  return String.fromCharCode(p1[0] === "x" ? parseInt(p1.slice(1), 16) : p1);
+    return String.fromCharCode(p1[0] === 'x' ? parseInt(p1.slice(1), 16) : p1);
 }
 
 window.onload = function() {
-  initialFetchBBS();
-  setInterval(fetchBBS, 10000);
-}
+    initialFetchBBS();
+    setInterval(fetchBBS, 10000);
+};
--- src/link.js
+++ src/link.js
@@ -2,6 +2,6 @@ var shell = require('electron').shell;
 
 //open links externally by default
 $(document).on('click', 'a[href^="http"]', function(event) {
-  event.preventDefault();
-  shell.openExternal(this.href);
+    event.preventDefault();
+    shell.openExternal(this.href);
 });
