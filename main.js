da9e5d4e90bc5d000bbc323b1c6f71854ed692e2
Kuhaku <kuhaku.mami@gmail.com>
2017-07-24 18:27:36 +0900

    eslint

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
-const Menu = electron.Menu
-const MenuItem = electron.MenuItem
-const menu = new Menu()
+const Menu = electron.Menu;
+const MenuItem = electron.MenuItem;
+const menu = new Menu();
 menu.append(new MenuItem({ label: 'Copy', role: 'copy', accelerator: 'CmdOrCtrl+C'}));
 menu.append(new MenuItem({ label: 'Paste', role: 'paste', accelerator: 'CmdOrCtrl+V'}));
 let mainWindow;
@@ -16,34 +16,34 @@ let mainWindow;
 app.once('window-all-closed',function() { app.quit(); });
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 400, height: 1000});
+    mainWindow = new BrowserWindow({width: 400, height: 1000});
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'src', 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'src', 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-  // デバッグ用
-  // mainWindow.webContents.openDevTools();
+    // デバッグ用
+    // mainWindow.webContents.openDevTools();
 
-  // メインウィンドウが閉じられたときの処理
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    // メインウィンドウが閉じられたときの処理
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
 
-  if (process.env.ELECTRON_IN_PAGE_SEARCH_DEBUG) {
-    mainWindow.webContents.openDevTools({mode: 'detach'});
-  }
+    if (process.env.ELECTRON_IN_PAGE_SEARCH_DEBUG) {
+        mainWindow.webContents.openDevTools({mode: 'detach'});
+    }
 }
 
 app.on('browser-window-created', function(event, win) {
-  win.webContents.on('context-menu', function(e, params) {
-    menu.popup(win, params.x, params.y)
-  })
-})
+    win.webContents.on('context-menu', function(e, params) {
+        menu.popup(win, params.x, params.y);
+    });
+});
 
-//  初期化が完了した時の処理
+//    初期化が完了した時の処理
 app.on('ready', createWindow);
 
 // 全てのウィンドウが閉じたときの処理
@@ -53,8 +53,8 @@ app.on('window-all-closed', function () {
 
 // アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
 app.on('activate', function () {
-  /// メインウィンドウが消えている場合は再度メインウィンドウを作成する
-  if (mainWindow === null) {
-    createWindow();
-  }
+    /// メインウィンドウが消えている場合は再度メインウィンドウを作成する
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
