ae0df21a83746dd242f51c969df5d93390b057cd
Kuhaku <kuhaku.mami@gmail.com>
2018-05-17 15:04:41 +0900

    Upgrade electron v1.7.12 -> v.2.0.1

--- binalize.sh
+++ binalize.sh
@@ -2,8 +2,8 @@
 
 rm -rf MisaoReader-*
 
-electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=1.7.12 --icon icons/misao.icns
-electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=1.7.12  --icon icons/misao.ico
+electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=2.0.1 --icon icons/misao.icns
+electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=2.0.1 --icon icons/misao.ico
 
 zip -r MisaoReader-darwin-x64.zip MisaoReader-darwin-x64
 zip -r MisaoReader-win32-x64.zip MisaoReader-win32-x64
