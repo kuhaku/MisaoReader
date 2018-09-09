83d7fd06864a7b3d4b7ea3f68138e805f7b8be06
Kuhaku <kuhaku.mami@gmail.com>
2018-09-10 00:56:43 +0900

    Change zip to dmg in macos

--- binalize.sh
+++ binalize.sh
@@ -5,5 +5,6 @@ rm -rf MisaoReader-*
 electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=2.0.1 --icon icons/misao.icns
 electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=2.0.1 --icon icons/misao.ico
 
-zip -r MisaoReader-darwin-x64.zip MisaoReader-darwin-x64
+ln -s /Applications MisaoReader-darwin-x64/
+hdiutil create MisaoReader-darwin-x64.dmg -volname "MisaoReader" -srcfolder "MisaoReader-darwin-x64"
 zip -r MisaoReader-win32-x64.zip MisaoReader-win32-x64
