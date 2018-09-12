c0967d52d4134da8754a251ce78d2174133d76b1
kuhaku <kuhaku.mami@gmail.com>
2018-09-13 08:53:41 +0900

    Bump electron-version to 2.0.8

--- binalize.sh
+++ binalize.sh
@@ -2,8 +2,8 @@
 
 rm -rf MisaoReader-*
 
-electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=2.0.1 --icon icons/misao.icns
-electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=2.0.1 --icon icons/misao.ico
+electron-packager . MisaoReader --platform=darwin --arch=x64 --overwrite --electron-version=2.0.8 --icon icons/misao.icns
+electron-packager . MisaoReader --platform=win32 --arch=x64 --overwrite --electron-version=2.0.8 --icon icons/misao.ico
 
 ln -s /Applications MisaoReader-darwin-x64/
 hdiutil create MisaoReader-darwin-x64.dmg -volname "MisaoReader" -srcfolder "MisaoReader-darwin-x64"
